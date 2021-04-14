bcb7322304db1bdfb17e705553126e1f51ba0ca0
Jeremy <jeremy1nz2000@gmail.com>
2021-04-14 15:33:15 +1200

    Initial route setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
+import './router.dart';
 
 void main() {
-  runApp(Text("Hello World!"));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return MaterialApp(
+      title: "The Wellbeing Protocol", //TODO: Un-hardcode
+      initialRoute: ROUTE_HOME,
+      routes: routes,
+    );
+  }
 }
