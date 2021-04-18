3aa37e9ba7db5cdacf15948acd9992caaa0bd30c
Deepa Mhatre <mhatredeep@AD.ECS.VUW.AC.NZ>
2021-04-19 01:09:29 +1200

    homescreen and profile, yet to finish

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -10,9 +10,87 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      drawerEdgeDragWidth: 0,
-      drawerEnableOpenDragGesture: false,
-    );
+        appBar: AppBar(
+          backgroundColor: const Color(0xFFFFAD8B),
+
+          //const Color(0xFFEFEFEF)
+          // title: Text("The Wellbeing Protocol"),
+        ),
+        drawer: NavDrawer(),
+        drawerEdgeDragWidth: 0,
+        drawerEnableOpenDragGesture: true,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFAD8B),
+                  // gradient: LinearGradient(
+                  //   colors: [Colors.green[200], Colors.yellow[200]],
+                  // ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // InkWell(
+                    //   onTap: () {
+                    //     Scaffold.of(context).openDrawer();
+                    //   },
+                    //   child: Icon(
+                    //     Icons.menu,
+                    //   ),
+                    // ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        "Hello DDjm",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        "Your Balance",
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              '20.00 \CAN', //String interpolation CAN is used
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                              //  Text(
+                              //   "20.00 BEE",
+                              // ),
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor: Colors.purple,
+                                child: Icon(Icons.qr_code))),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@
 //   runApp(Text("Hello World!"));
 // }
 
-
 import 'package:flutter/material.dart';
- 
+import 'package:the_wellbeing_protocol/features/home/home_screen.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: HomeScreen
+      home: HomeScreen(),
     );
   }
 }
