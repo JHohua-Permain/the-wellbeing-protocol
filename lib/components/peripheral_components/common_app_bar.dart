e3a101392c6bf752a5665f7e16bf3e4f580ca0d5
Deepa Mhatre <mhatredeep@AD.ECS.VUW.AC.NZ>
2021-05-05 14:00:26 +1200

    removed leading backArrow Icon from Community fund and shop view

--- lib/components/peripheral_components/common_app_bar.dart
+++ lib/components/peripheral_components/common_app_bar.dart
@@ -1,50 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:the_wellbeing_protocol/components/components.dart';
 
-import '../../_mocks.dart';
-
+// ignore: must_be_immutable
 class CommonAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   final Size preferredSize = Size.fromHeight(kToolbarHeight);
   final String title;
   final VoidCallback toInfoScreen;
+  bool showleading = true;
 
-  CommonAppBar({this.title, this.toInfoScreen});
+  CommonAppBar({this.title, this.toInfoScreen, this.showleading});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title, style: TextStyle(color: Colors.black)),
       shape: Border(bottom: BorderSide(width: 2)),
-      automaticallyImplyLeading: false,
-      leading: InkWell(
-        onTap: () {
-          Navigator.of(context).pop();
-          // Navigator.of(context).push(MaterialPageRoute(
-          //     builder: (context) => WalletScreen(
-          //         drawerName: testAccount.name,
-          //         drawerPublicAddress: testAccount.publicAddress)));
-
-          //Navigator.popUntil(context, defaultR => false);
-          //Navigator.of(context).push();
-          //Navigator.pushNamed(context, '/');
-          // Navigator.push(
-          //     context,
-          //     MaterialPageRoute(
-          //         builder: (context) => WalletScreen(
-          //             drawerName: testAccount.name,
-          //             drawerPublicAddress: testAccount.publicAddress)));
-
-          //MaterialPageRoute(
-          // builder: (context) => WalletScreen(
-          //     drawerName: testAccount.name,
-          //     drawerPublicAddress: testAccount.publicAddress))
-        },
-        child: Icon(
-          Icons.arrow_back_ios,
-          color: Colors.black,
-        ),
-      ),
+      leading: showleading == true
+          ? InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              ),
+            )
+          : null,
       actions: [
         IconButton(
           icon: Icon(Icons.info),
