8a9dc6ce41a2de5c74efa52a65c2896660880c29
neha.sharawat01@gmail.com <neha.sharawat01@gmail.cim>
2021-05-14 02:47:02 +1200

    select language widget created

--- lib/widgets/select_language.dart
+++ lib/widgets/select_language.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SettingPage extends StatelessWidget {
+class SelectLanguage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,40 +37,30 @@ class SettingPage extends StatelessWidget {
                   // );
                 },
               ),
-              ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 30),
-                  child: Text('Language'),
+              Padding(
+                padding: const EdgeInsets.all(3),
+                child: Divider(
+                  height: 5,
                 ),
+              ),
+              ListTile(
+                title: Text('Language'),
                 trailing: InkWell(
                   onTap: () => Navigator.of(context).pop(),
                   child: Icon(
-                    Icons.arrow_drop_down,
+                    Icons.arrow_drop_up,
                     color: Colors.black,
                   ),
                 ),
-                onTap: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => SelectLanguage(),
-                  //       ),
-                  // );
-                },
               ),
               ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(top: 30, bottom: 30),
-                  child: Text('Logout'),
-                ),
-                onTap: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => LogoutScreen(),
-                  //       ),
-                  // );
-                },
+                title: Text('United State'),
+              ),
+              ListTile(
+                title: Text('Espana'),
+              ),
+              ListTile(
+                title: Text('Brasil'),
               ),
             ]).toList(),
       ),
