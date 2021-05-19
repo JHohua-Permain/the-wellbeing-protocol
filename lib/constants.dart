9633c54cb47b59fe8c94662b65b6ce311f0e89c2
Jeremy <jeremy1nz2000@gmail.com>
2021-05-19 14:43:57 +1200

    Add constants.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,29 @@
-const String APP_NAME = "The Wellbeing Project";
-const String FUSE_RPC_URL = "https://rpc.fuse.io";
-const int FUSE_CHAIN_ID = 122;
+/// The constants used throughout the app.
+
+const APP_NAME = "The Wellbeing Protocol";
+
+///The base uri used to connect to the Fuse Studio backend server.
+const API_BASE_URL = "https://studio.fuse.io/api";
+
+///The uri used to connect to Fuse's RPC server.
+const FUSE_RPC_URL = "https://rpc.fuse.io";
+
+///The Fuse Network/Blockchain's id compared to other Ethereum based Networks/Blockchain id's.
+const FUSE_CHAIN_ID = 122;
+
+///The uri to connect to the Fuse sub-graph.
+const GRAPH_BASE_URL = 'https://graph.fuse.io/subgraphs/name/fuseio';
+
+//Fuse economy and community addresses.
+
+const DEFAULT_COMMUNITY_ADDRESS = JEREMY_ECONOMY_COMMUNITY_ADDRESS;
+
+//Jeremy's Economy addresses
+const JEZ_TOKEN_ADDRESS = "0xe8CD1134F1B4Ef599Fa1F2e3B181F2A2D98E8c8C";
+const JEREMY_ECONOMY_COMMUNITY_ADDRESS =
+    "0x858bce60ffA86cba96DbE47Dde8A87ed7E9DfBF8";
+
+//Ben's Economy addresses
+const BEN_TOKEN_ADDRESS = "0xf2E83701aD3e0131636D53b93B32167d33b925cD";
+const BEN_ECONOMY_COMMUNITY_ADDRESS =
+    "0x19e8b1CdF762Eef5Eeb149482f6F4ffC2d894910";
