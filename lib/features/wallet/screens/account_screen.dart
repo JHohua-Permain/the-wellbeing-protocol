22e272e21d975e781b203f504c1abd8dedc2aef6
neha.sharawat01@gmail.com <neha.sharawat01@gmail.cim>
2021-06-09 15:16:11 +1200

    Account screen fixed

--- lib/features/wallet/screens/account_screen.dart
+++ lib/features/wallet/screens/account_screen.dart
@@ -18,79 +18,85 @@ class _AccountScreenState extends State<AccountScreen> {
   Widget build(BuildContext context) {
     return AppScaffold(
       title: 'Account',
-      body: Column(
-        children: [
-          SizedBox(height: 10),
-          CircleAvatar(
-            radius: 40,
-            backgroundColor: Colors.grey[350],
-            child: Icon(
-              Icons.edit_rounded,
-              color: Colors.black38,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(height: 10),
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.grey[350],
+              child: Icon(
+                Icons.edit_rounded,
+                color: Colors.black38,
+              ),
             ),
-          ),
-          Text(
-            widget.vm.displayName,
-            style: TextStyle(
-              color: Colors.grey[900],
-              fontSize: 12,
+            Text(
+              widget.vm.displayName,
+              style: TextStyle(
+                color: Colors.grey[900],
+                fontSize: 12,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Divider(
-              color: Colors.grey,
-              height: 1,
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Divider(
+                color: Colors.grey,
+                height: 1,
+              ),
             ),
-          ),
-          ListTile(
-            contentPadding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            title: Text(
-              'Name',
-              style: TextStyle(fontSize: 12, color: Colors.grey[950]),
-            ),
-            subtitle: TextFormField(
-              controller: _displayNameController,
-              decoration: InputDecoration(
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(top: 10, left: 10, right: 10),
+              title: Text(
+                'Name',
+                style: TextStyle(fontSize: 12, color: Colors.grey[950]),
+              ),
+              subtitle: TextFormField(
+                controller: _displayNameController,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () => widget.vm.handleDisplayNameChange(
+                  _displayNameController.value.text,
+                ),
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.grey[950],
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () => widget.vm
-                  .handleDisplayNameChange(_displayNameController.value.text),
-              icon: Icon(
-                Icons.edit,
+            Divider(color: Colors.grey, height: 1),
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(top: 10, left: 10, right: 10),
+              title: Text(
+                'Wallet Address',
+                style: TextStyle(fontSize: 12, color: Colors.grey[950]),
+              ),
+              subtitle: Text(widget.vm.walletAddress),
+              trailing: Icon(
+                Icons.copy,
+                size: 15,
                 color: Colors.grey[950],
               ),
             ),
-          ),
-          Divider(color: Colors.grey, height: 1),
-          ListTile(
-            contentPadding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            title: Text(
-              'Wallet Address',
-              style: TextStyle(fontSize: 12, color: Colors.grey[950]),
+            Divider(color: Colors.grey, height: 1),
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(top: 10, left: 10, right: 10),
+              title: Text(
+                'Phone Number',
+                style: TextStyle(fontSize: 12, color: Colors.grey[950]),
+              ),
+              subtitle: Text(widget.vm.primaryContactNum),
             ),
-            subtitle: Text(widget.vm.walletAddress),
-            trailing: Icon(
-              Icons.copy,
-              size: 15,
-              color: Colors.grey[950],
-            ),
-          ),
-          Divider(color: Colors.grey, height: 1),
-          ListTile(
-            contentPadding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            title: Text(
-              'Phone Number',
-              style: TextStyle(fontSize: 12, color: Colors.grey[950]),
-            ),
-            subtitle: Text(widget.vm.primaryContactNum),
-          ),
-          Divider(color: Colors.grey, height: 1),
-        ],
+            Divider(color: Colors.grey, height: 1),
+          ]),
+        ),
       ),
     );
   }
