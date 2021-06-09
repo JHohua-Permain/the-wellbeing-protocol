3b537ff1d7fca3df7305e0ad59181d658b0d23a7
neha.sharawat01@gmail.com <neha.sharawat01@gmail.cim>
2021-06-09 14:24:54 +1200

    wallet screen fixed

--- lib/features/wallet/screens/backup_wallet_screen.dart
+++ lib/features/wallet/screens/backup_wallet_screen.dart
@@ -14,81 +14,90 @@ class BackupWalletScreen extends StatelessWidget {
       title: 'Backup your Wallet',
       body: Container(
         child: Column(
-          children: <Widget>[
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 25),
-                child: Text(
-                  'Important! write down the words in order and\n keep them safe. You won\'t be able to recover your\n account without it.',
-                  textAlign: TextAlign.center,
-                ),
-              ),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              height: 5,
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 40,
-                right: 40,
-                top: 30,
-                bottom: 40,
-              ),
-              child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 5),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 350,
-                        child: GridView.count(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 40.0,
-                          // mainAxisSpacing: 4,
-                          children: List.generate(
-                            vm.mnemonic.length,
-                            (index) => Center(
-                              child: Text('$index. ${vm.mnemonic[index]}'),
+            Text(
+                'Important! write down the words in order and\n keep them safe. You won\'t be able to recover your\n account without it.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal)),
+            SizedBox(
+              height: 10,
+            ),
+            Column(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: Container(
+                    padding: EdgeInsets.only(
+                        left: 10, right: 10, top: 10, bottom: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 4),
+                      color: Color(0xFFF7F7F7),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15.0),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 250,
+                          child: GridView.count(
+                            crossAxisCount: 3,
+                            crossAxisSpacing:65.0,
+                            children: List.generate(
+                              vm.mnemonic.length,
+                                  (index) => Center(
+                                child: Text('$index. ${vm.mnemonic[index]}'),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      // RaisedButton.icon(
-                      // color: Colors.blue,
-                      Padding(
-                        padding: const EdgeInsets.only(left: 90, bottom: 30),
-                        child: Row(children: <Widget>[
-                          Text("Copy to clipboard  "),
-                          Icon(Icons.copy),
-                        ]),
-                      )
-                    ],
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Material(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    color: Color(0xFF801E48),
-                    clipBehavior: Clip.antiAlias,
-                    child: MaterialButton(
-                      minWidth: 218.0,
-                      height: 50,
-                      color: const Color(0xFFFFAD8B),
-                      child: Text(
-                        'Next',
-                        style: TextStyle(fontSize: 16.0, color: Colors.black),
-                      ),
-                      onPressed: () {},
-                    ),
-                  )
-                ],
+                        // RaisedButton.icon(
+                        // color: Colors.blue,
+                        Padding(
+                          padding: const EdgeInsets.only(left: 90, bottom: 10, top:15),
+                          child: Row(children: <Widget>[
+                            Text(
+                              "Copy to clipboard  ",
+                              style: TextStyle(color: Colors.lightBlue),
+                            ),
+                            Icon(Icons.copy),
+                          ]),
+                        )
+                      ],
+                    )),
               ),
+            ]),
+            SizedBox(
+              height: 20,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Material(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Color(0xFF801E48),
+                  clipBehavior: Clip.antiAlias,
+                  child: MaterialButton(
+                    minWidth: 218.0,
+                    height: 50,
+                    color: const Color(0xFFFFAD8B),
+                    child: Text(
+                      'Next',
+                      style: TextStyle(fontSize: 16.0, color: Colors.black),
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            )
           ],
         ),
       ),
